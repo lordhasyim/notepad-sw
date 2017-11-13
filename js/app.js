ae0f2a981dd1b55fc234e1854452ec39d50d4185
Hasyim <lordhasyim@gmail.com>
2017-11-13 10:50:59 +0700

    :bug: fix js not error typo

--- js/app.js
+++ js/app.js
@@ -1,21 +1,44 @@
+
+$(document).ready(function(){
+  $('#note').keyup(debounce(function(){
+    localStorage.setItem("note", $(this).val());
+    console.log('dbounce test..');
+    $("#flash").removeClass('hidden');
+  },500));
+
+  if(localStorage.getItem("note") && localStorage.getItem("note")!=''){
+    var noteItem = localStorage.getItem("note")
+    $('#note').val(noteItem);
+  }
+});
+
+setInterval(function() {
+      $("#flash").addClass('hidden');
+      //alert('test');
+  }, 5000);
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
 //Registering service worker
 if ('serviceWorker' in navigator){
 	navigator.serviceWorker.register('sw.js').then(function (registration){
 		//Registration was successfull
-		console.log('ServiceWorker registration successfull with scope : ', registration.scoper);
-	})catch.(function(err){
+		console.log('ServiceWorker registration successfull with scope : ', registration.scope);
+	}).catch(function(err){
 		//registration failed
 		console.log('serviceWorker registration failed : ', err);
 	});
 }
-
-$(document).ready(function(){
-	$('#note').bind('input propertychange', function(){
-		localstorage.setItem("note", $(this).val());
-	});
-
-	if (localstorage.getItem("note") && localstorage.getItem("note") !==''){
-		var noteItem = localstorage.getItem("note");
-		$('#note').val(noteItem);
-	}
-})
--- sw.js
+++ sw.js
@@ -5,6 +5,9 @@ var CACHE_FILE = [
 	'/',
 	'index.html',
 	'js/app.js',
+    'js/jquery.min.js',
+    'js/bootstrap.min.js',
+    'css/bootstrap.min.css',
 	'css/style.css',
 	'favicon.ico',
 	'manifest.json',
@@ -39,13 +42,13 @@ self.addEventListener('fetch', function(event) {
 function requesetBackend(event) {
 
     var url = event.request.clone();
-    return fetch(url).then(function(res) {\
+    return fetch(url).then(function(res) {
         //if not valid request send error
         if (!res | res.status !== 200 || res.type !== 'basic') {
             return res;
         }
 
-        var response = res.clone;
+        var response = res.clone();
 
         caches.open(CACHE_VERSION).then(function(cache) {
             cache.put(event.request, response);
@@ -59,7 +62,7 @@ function requesetBackend(event) {
 self.addEventListener('activate', function(event){
 	event.wainUntil(
 		caches.keys().then(function(keys){
-			return promise.al(keys.map(function(key, i){
+			return promise.all(keys.map(function(key, i){
 				if(key !== CACHE_VERSION){
 					retrun caches.delete(keys[i]);
 				}
